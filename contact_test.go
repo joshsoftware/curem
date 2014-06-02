34fdd21165bfd9d0b80dcdc30146e6576a48d2fb
Hari haran <hariharan.uno@gmail.com>
2014-06-02 11:29:13 +0530

    Add tests for GetContact() function

--- contact_test.go
+++ contact_test.go
@@ -44,3 +44,37 @@ func TestNewContact(t *testing.T) {
 		t.Errorf("%s", err)
 	}
 }
+
+func TestGetContact(t *testing.T) {
+	sess, err := mgo.Dial("localhost")
+	if err != nil {
+		t.Errorf("%s", err)
+	}
+	defer sess.Close()
+	sess.SetSafe(&mgo.Safe{})
+	collection := sess.DB("test").C("newcontact")
+	fakeContact, err := NewContact(
+		collection,
+		"Encom Inc.",
+		"Flynn",
+		"flynn@encom.com",
+		"",
+		"",
+		"USA",
+	)
+	if err != nil {
+		t.Errorf("%s", err)
+	}
+	id := fakeContact.Id
+	fetchedContact, err := GetContact(id)
+	if err != nil {
+		t.Errorf("%s", err)
+	}
+	if *fakeContact != *fetchedContact {
+		t.Errorf("Expected %+v, but got %+v\n", *fakeContact, *fetchedContact)
+	}
+	err = collection.DropCollection()
+	if err != nil {
+		t.Errorf("%s", err)
+	}
+}
