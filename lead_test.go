2a75e81dc9c3e2ccb2f059b57264611ae6bb10f7
Hari haran <hariharan.uno@gmail.com>
2014-06-02 11:54:26 +0530

    Add tests for DeleteLead()

--- lead_test.go
+++ lead_test.go
@@ -126,3 +126,62 @@ func TestGetLead(t *testing.T) {
 		t.Errorf("%s", err)
 	}
 }
+
+func TestDeleteLead(t *testing.T) {
+	sess, err := mgo.Dial("localhost")
+	if err != nil {
+		t.Errorf("%s", err)
+	}
+	defer sess.Close()
+	sess.SetMode(mgo.Monotonic, true)
+	sess.SetSafe(&mgo.Safe{})
+	collection := sess.DB("test").C("newlead")
+	f := fakeContactId(sess)
+	fakeLead, err := NewLead(
+		collection,
+		&mgo.DBRef{
+			Collection: "newcontact",
+			Id:         f,
+			Database:   "test",
+		},
+		"Web",
+		"Hari",
+		"Warming Up",
+		2.5,
+		20,
+		3,
+		"25th June, 2014",
+		[]string{"Call back", "Based in mumbai"},
+	)
+	if err != nil {
+		t.Errorf("%s", err)
+	}
+	id := fakeLead.Id
+	err = DeleteLead(id)
+	if err != nil {
+		t.Errorf("%s", err)
+	}
+	n, err := collection.Count()
+	if err != nil {
+		t.Errorf("%s", err)
+	}
+	if n != 0 {
+		t.Errorf("expected 0 documents in the collection, but found %d", n)
+	}
+	err = collection.DropCollection()
+	if err != nil {
+		t.Errorf("%s", err)
+	}
+	contactCollection := sess.DB("test").C("newcontact")
+	n, err = contactCollection.Count()
+	if err != nil {
+		t.Errorf("%s", err)
+	}
+	if n != 1 {
+		t.Errorf("expected 1 document in the collection, but found %d", n)
+	}
+	err = contactCollection.DropCollection()
+	if err != nil {
+		t.Errorf("%s", err)
+	}
+}
