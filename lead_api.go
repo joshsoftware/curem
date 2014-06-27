4e8e0de77a07075ed1bcb6d41cb38c3542fb7bb8
Hari haran <hariharan.uno@gmail.com>
2014-06-27 16:40:06 +0530

    Add getLeadHandler

--- lead_api.go
+++ lead_api.go
@@ -4,15 +4,36 @@ import (
 	"encoding/json"
 	"log"
 	"net/http"
+
+	"github.com/gorilla/mux"
+	"labix.org/v2/mgo/bson"
 )
 
 func init() {
 	r.HandleFunc("/leads", getLeadsHandler).Methods("GET")
+	r.HandleFunc("/leads/{id}", getLeadHandler).Methods("GET")
 }
 
 func getLeadsHandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	c, err := GetAllLeads()
+	if err != nil {
+		log.Println(err)
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+	enc := json.NewEncoder(w)
+	if err = enc.Encode(c); err != nil {
+		log.Println(err)
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+}
+
+func getLeadHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	vars := mux.Vars(r)
+	i := vars["id"]
+	id := bson.ObjectIdHex(i)
+	c, err := GetLead(id)
 	if err != nil {
 		log.Println(err)
 		http.Error(w, err.Error(), http.StatusInternalServerError)
