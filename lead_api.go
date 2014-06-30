af728fbef8eaea121c113abc4b16c8fecbbecf1b
Hari haran <hariharan.uno@gmail.com>
2014-06-30 12:49:24 +0530

    Add patchLeadHandler

--- lead_api.go
+++ lead_api.go
@@ -12,6 +12,7 @@ import (
 func init() {
 	r.HandleFunc("/leads", getLeadsHandler).Methods("GET")
 	r.HandleFunc("/leads/{id}", getLeadHandler).Methods("GET")
+	r.HandleFunc("/leads/{id}", patchLeadHandler).Methods("PATCH")
 }
 
 func getLeadsHandler(w http.ResponseWriter, r *http.Request) {
@@ -42,5 +43,36 @@ func getLeadHandler(w http.ResponseWriter, r *http.Request) {
 	if err = enc.Encode(c); err != nil {
 		log.Println(err)
 		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+}
+
+func patchLeadHandler(w http.ResponseWriter, r *http.Request) {
+	decoder := json.NewDecoder(r.Body)
+	var l incomingLead
+	err := decoder.Decode(&l)
+	if err != nil {
+		log.Println(err)
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	vars := mux.Vars(r)
+	id := bson.ObjectIdHex(vars["id"])
+	fl, err := GetLead(id)
+	if err != nil {
+		log.Println(err)
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	err = fl.copyIncomingFields(&l)
+	if err != nil {
+		log.Println(err)
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	err = fl.Update()
+	if err != nil {
+		log.Println(err)
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
 	}
 }
