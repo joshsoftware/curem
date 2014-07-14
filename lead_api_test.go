4e6dd8e625e55bb2d44d04066c2bba54f4145f5c
Hari haran <hariharan.uno@gmail.com>
2014-07-14 12:56:46 +0530

    Add test for deleteLeadHandler

--- lead_api_test.go
+++ lead_api_test.go
@@ -86,3 +86,40 @@ func TestGetLeadHandler(t *testing.T) {
 	}
 	dropCollections(t)
 }
+
+func TestDeleteLeadHandler(t *testing.T) {
+	ts := httptest.NewServer(r)
+	defer ts.Close()
+	c, err := NewContact(
+		"Encom Inc.",
+		"Flynn",
+		"flynn@encom.com",
+		"",
+		"",
+		"USA",
+	)
+	if err != nil {
+		t.Errorf("%s", err)
+	}
+	l, err := NewLead(c.Slug, "Web", "Gautam", "Won", 3, 5, 2, "3rd July, 2014", nil)
+	req, err := http.NewRequest("DELETE", ts.URL+"/leads/"+l.ID.Hex(), nil)
+	if err != nil {
+		t.Errorf("%s", err)
+	}
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		t.Errorf("%s", err)
+	}
+	defer resp.Body.Close()
+	if resp.StatusCode != 204 {
+		t.Errorf("expected response status code to be 204, but got %d", resp.StatusCode)
+	}
+	x, err := GetAllLeads()
+	if err != nil {
+		t.Errorf("%s", err)
+	}
+	if len(x) != 0 {
+		t.Errorf("expected 0 leads after delete, but got %d", len(x))
+	}
+	dropCollections(t)
+}
